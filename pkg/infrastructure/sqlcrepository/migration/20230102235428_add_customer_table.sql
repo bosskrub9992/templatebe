acaa938f06d06f297c91b084c59f03ceeb48f3c2
jinleejun-corp <jinleejun-corp@gmail.com>
2023-01-03 02:05:30 +0700

    REF: remove comment statement

--- pkg/infrastructure/sqlcrepository/migration/20230102235428_add_customer_table.sql
+++ pkg/infrastructure/sqlcrepository/migration/20230102235428_add_customer_table.sql
@@ -1,12 +1,8 @@
 -- +goose Up
--- +goose StatementBegin
 CREATE TABLE customers (
   id   BIGSERIAL PRIMARY KEY,
   name text      NOT NULL
 );
--- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
 DROP TABLE IF EXISTS customers;
--- +goose StatementEnd
