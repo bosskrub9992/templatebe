ccd61c8a85eb28e20347cd152919f63ca8678a94
jinleejun-corp <jinleejun-corp@gmail.com>
2023-02-28 19:38:03 +0700

    FIX: unit test

--- service/bff/src/controller/customer_test.go
+++ service/bff/src/controller/customer_test.go
@@ -1,72 +1,73 @@
 package controller
 
-// import (
-// 	"context"
-// 	"github.com/bosskrub9992/templatebe/service/bff/lib/loggers"
-// 	"github.com/bosskrub9992/templatebe/service/bff/src/config"
-// 	"github.com/bosskrub9992/templatebe/service/bff/src/model"
-// 	"github.com/bosskrub9992/templatebe/service/bff/src/repository/mockrepository"
-// 	"reflect"
-// 	"testing"
+import (
+	"context"
+	"reflect"
+	"testing"
 
-// 	"github.com/golang/mock/gomock"
-// )
+	"github.com/bosskrub9992/templatebe/corelib/loggers"
+	"github.com/bosskrub9992/templatebe/service/bff/src/config"
+	"github.com/bosskrub9992/templatebe/service/bff/src/model"
+	"github.com/bosskrub9992/templatebe/service/bff/src/repository/mockrepository"
 
-// func TestCustomerController_CreateCustomer(t *testing.T) {
-// 	mockCtrl := gomock.NewController(t)
-// 	defer mockCtrl.Finish()
+	"github.com/golang/mock/gomock"
+)
 
-// 	cfg := config.New()
-// 	logger := loggers.NewZerolog(cfg)
-// 	mockCustomerRepo := mockrepository.NewMockCustomerRepository(mockCtrl)
+func TestCustomerController_CreateCustomer(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
 
-// 	type args struct {
-// 		ctx context.Context
-// 		req model.CreateCustomerRequest
-// 	}
-// 	tests := []struct {
-// 		name    string
-// 		con     *CustomerController
-// 		args    args
-// 		want    *model.CreateCustomerResponse
-// 		wantErr bool
-// 		mock    func()
-// 	}{
-// 		{
-// 			name: "success",
-// 			con: &CustomerController{
-// 				logger:             logger,
-// 				customerRepository: mockCustomerRepo,
-// 			},
-// 			args: args{
-// 				ctx: context.Background(),
-// 				req: model.CreateCustomerRequest{
-// 					ID:   1,
-// 					Name: "Boss",
-// 				},
-// 			},
-// 			want: &model.CreateCustomerResponse{
-// 				ID: 1,
-// 			},
-// 			wantErr: false,
-// 			mock: func() {
-// 				mockCustomerRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(int64(1), nil)
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if tt.mock != nil {
-// 				tt.mock()
-// 			}
-// 			got, err := tt.con.CreateCustomer(tt.args.ctx, tt.args.req)
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("CustomerController.CreateCustomer() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("CustomerController.CreateCustomer() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+	zerologConfig := config.NewLoggerConfig()
+	logger := loggers.NewZerolog(zerologConfig)
+	mockCustomerRepo := mockrepository.NewMockCustomerRepository(mockCtrl)
+
+	type args struct {
+		ctx context.Context
+		req model.CreateCustomerRequest
+	}
+	tests := []struct {
+		name    string
+		con     *CustomerController
+		args    args
+		want    *model.CreateCustomerResponse
+		wantErr bool
+		mock    func()
+	}{
+		{
+			name: "success",
+			con: &CustomerController{
+				logger:             logger,
+				customerRepository: mockCustomerRepo,
+			},
+			args: args{
+				ctx: context.Background(),
+				req: model.CreateCustomerRequest{
+					ID:   1,
+					Name: "Boss",
+				},
+			},
+			want: &model.CreateCustomerResponse{
+				ID: 1,
+			},
+			wantErr: false,
+			mock: func() {
+				mockCustomerRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(int64(1), nil)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.mock != nil {
+				tt.mock()
+			}
+			got, err := tt.con.CreateCustomer(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CustomerController.CreateCustomer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CustomerController.CreateCustomer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
