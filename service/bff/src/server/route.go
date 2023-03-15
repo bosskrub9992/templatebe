3106784f123cdd9ae7d95595777704d1a03ec6c8
jinleejun-corp <jinleejun-corp@gmail.com>
2023-03-15 09:32:56 +0700

    REFACTOR: delete route.go

--- service/bff/src/server/route.go
+++
@@ -1,8 +0,0 @@
-package server
-
-func (r *RESTServer) RegisterRoute() {
-	v1Group := r.e.Group("/api/v1")
-
-	v1Group.GET("/health", r.handler.GetHealth)
-	v1Group.POST("/customers", r.handler.CreateCustomer)
-}
