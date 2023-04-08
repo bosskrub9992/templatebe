73e8b3d5a2f466a6f9cc13e4878673b6fe1fa7fe
jinleejun-corp <jinleejun-corp@gmail.com>
2023-04-08 15:42:07 +0700

    DELETE not used func

--- corelib/errs/errs.go
+++ corelib/errs/errs.go
@@ -3,7 +3,6 @@ package errs
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/spf13/viper"
 )
@@ -17,14 +16,6 @@ type Err struct {
 
 func (e *Err) Error() string {
 	return e.Message
-}
-
-func New(code Code, err error) Err {
-	return Err{
-		Code:    code,
-		Message: viper.GetString(strconv.Itoa(int(code))),
-		Errors:  err.Error(),
-	}
 }
 
 func NewBind(err error) Err {
